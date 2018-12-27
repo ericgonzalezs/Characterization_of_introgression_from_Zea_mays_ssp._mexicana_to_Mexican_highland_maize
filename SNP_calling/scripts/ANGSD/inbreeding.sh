3e37796388c2e1e767de3691ff6116a1f5b628fb
ericgonzalezs <shinken123@gmail.com>
2018-12-27 22:49:53 +0100

    Update inbreeding.sh

--- SNP_calling/scripts/ANGSD/inbreeding.sh
+++ SNP_calling/scripts/ANGSD/inbreeding.sh
@@ -10,13 +10,12 @@ NP=$(wc -l < $PBS_NODEFILE)
 module load ngsF/1.2.0
 module load ANGSD/0.912
 
-angsd -GL 1 -bam bam_list.txt -remove_bads 1 -nThreads 8  -doGlf 3 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -minInd 4 -minMapQ 30 -minQ 20 -out 
-GL_con_Par.HWE
+angsd -GL 1 -bam bam_list.txt -remove_bads 1 -nThreads 8  -doGlf 3 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -minInd 4 -minMapQ 30 -minQ 20 -out GL_con_Par.HWE
 
-#se contaron los sitios con zcat GL_con_Par.HWE.mafs.gz | wc -l y se le resto 1
+#the sites were counted with this:  zcat GL_con_Par.HWE.mafs.gz | wc -l 
+#and it was usbtracted 1 from teh count 
 
-#se descomprimio el archivo glf con esto:
-zcat GL_con_Par.HWE.glf.gz
+#The file was deocmpressed with: zcat GL_con_Par.HWE.glf.gz
 
 ngsF10.sh --n_threads 20 --n_ind 12 --min_epsilon 1e-6 --n_sites 78818598 --glf GL_con_Par.HWE.glf --out Con_par
                                                                  
